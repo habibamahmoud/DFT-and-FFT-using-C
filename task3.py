89b30fb5c470c6a6053909afa2d4d71da493eef5
habibamahmoud <habiba.ahmed98@eng-st.cu.edu.eg>
2021-05-26 23:26:35 +0200

    Edit

--- task3.py
+++ task3.py
@@ -136,42 +136,15 @@ class Ui_MainWindow(object):
         # doublesArray === double [4];
         # arr2 = doublesArray.from_address(np_array) ---- arr2 is double arr[4] in C
         self.N = np.array([1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024], dtype=np.int)
+        #self.N = np.array([16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384])
         self.i_real = np.arange((1024), dtype=np.double)
         self.i_imag = np.arange((1024), dtype=np.double)
+        self.dftTime = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], dtype= np.double)
+        self.fftTime = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], dtype= np.double)
         self.DFT.clicked.connect(lambda: self.plotting(index = 1))
         self.FFT.clicked.connect(lambda: self.plotting(index = 2))
         self.clear.clicked.connect(lambda: self.plotting(index = 3))
-        self.error.clicked.connect(lambda: self.plotting(index = 4))
-        self.dftTime = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], dtype= np.double)
-        self.fftTime = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], dtype= np.double)
-        self.i_size = len(self.i_real)
-        doublesArray = ctypes.c_double * self.i_size
-        i_real_C = doublesArray.from_address(self.i_real.ctypes.data)
-        i_imag_C = doublesArray.from_address(self.i_imag.ctypes.data)
-
-        o_real_ptr = ctypes.POINTER(ctypes.c_double)()
-        o_imag_ptr = ctypes.POINTER(ctypes.c_double)()
-        o_size = ctypes.c_int()
-
-        library.C_fft(i_real_C, i_imag_C, self.i_size, ctypes.byref(o_real_ptr), ctypes.byref(o_imag_ptr), ctypes.byref(o_size))
-
-
-        fft_o_real = o_real_ptr[:o_size.value]
-        fft_o_imag = o_imag_ptr[:o_size.value]
-
-        
-
-        library.C_dft(i_real_C, i_imag_C, self.i_size, ctypes.byref(o_real_ptr), ctypes.byref(o_imag_ptr), ctypes.byref(o_size))
-        
-        dft_o_real = o_real_ptr[:o_size.value]
-        dft_o_imag = o_imag_ptr[:o_size.value]
-
-        error_real =  np.subtract(dft_o_real ,fft_o_real)
-        error_imag =  np.subtract(dft_o_imag ,fft_o_imag)
-        error = np.multiply(error_real, error_imag)
-        self.errorArray = error[0: 11]
-
-        print("error :",self.errorArray)
+        self.error.clicked.connect(self.showError)
 # g++ -fPIC -shared -o libfft.so fft.cpp && python run.py
 
 
@@ -206,8 +179,37 @@ class Ui_MainWindow(object):
         if (index == 2):
             print(self.fftTime)
             self.widget.plot(self.N, self.fftTime, name = "FFT", pen = "b")
-        if (index == 4):
-            self.widget_2.plot(self.N, self.errorArray, name = "Error", pen = "r")
+    def showError(self):
+        self.i_size = len(self.i_real)
+        doublesArray = ctypes.c_double * self.i_size
+        i_real_C = doublesArray.from_address(self.i_real.ctypes.data)
+        i_imag_C = doublesArray.from_address(self.i_imag.ctypes.data)
+
+        o_real_ptr = ctypes.POINTER(ctypes.c_double)()
+        o_imag_ptr = ctypes.POINTER(ctypes.c_double)()
+        o_size = ctypes.c_int()
+
+        library.C_fft(i_real_C, i_imag_C, self.i_size, ctypes.byref(o_real_ptr), ctypes.byref(o_imag_ptr), ctypes.byref(o_size))
+
+
+        fft_o_real = o_real_ptr[:o_size.value]
+        fft_o_imag = o_imag_ptr[:o_size.value]
+
+        
+
+        library.C_dft(i_real_C, i_imag_C, self.i_size, ctypes.byref(o_real_ptr), ctypes.byref(o_imag_ptr), ctypes.byref(o_size))
+        
+        dft_o_real = o_real_ptr[:o_size.value]
+        dft_o_imag = o_imag_ptr[:o_size.value]
+
+        error_real =  np.subtract(dft_o_real ,fft_o_real)
+        error_imag =  np.subtract(dft_o_imag ,fft_o_imag)
+        error = np.multiply(error_real, error_imag)
+        errorArray = error[0: 11]
+
+        print("error :", errorArray)
+        self.widget_2.plot(self.N, errorArray, name = "Error", pen = "r")
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "DFT vs FFT"))
